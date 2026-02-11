c0004c6fc9320d4346a7a9fe23ee769c01890280
User <mimi@nyamugo.com>
2026-02-11 08:08:18 +0300

    update files

--- src/NGemi/Ngemi.jsx
+++ src/NGemi/Ngemi.jsx
@@ -7,13 +7,21 @@ const Ngemi = () => {
     <div className='ngemi'>
         <Navbar/>
         <h5>NGEMI</h5>
+        <h6>
+          Thaayú mbarí ya nyamúgo, ici ní ngemi iria ciugagwo mwana aciarwo.
+           Mwana wa kahíí ciake níciothe ithano no wa kairítu ní inya.
+          
+        </h6>
         <ul>
         
     <li>    1.  Úgo - úgí </li>
 <li>2.  Úrathi - </li>
 <li>3.  Úthamaki </li>
 <li>4.  Útonga </li>
-<li>5.  Úcamba</li>
+<li>5.  Úcamba - <p>
+   Ya úcamba ndiugagírwo wa kairítu níúndú írongoragírio 
+           mwana wa kahíí akorwo na úcamba wa gúthií ita gútaha.
+  </p></li>
   </ul>
     </div>
   )
